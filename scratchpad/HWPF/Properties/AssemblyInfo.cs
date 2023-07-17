7987ec8b88225b4cc6fbe2db661be70b2c4323ea
IS4 <illidans4@gmail.com>
2023-07-17 15:49:24 +0200

    remove AllowPartiallyTrustedCallers (fixes "Inheritance security rules violated by type: 'NPOI.HWPF.HWPFDocumentCore'")

--- scratchpad/HWPF/Properties/AssemblyInfo.cs
+++ scratchpad/HWPF/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -10,5 +9,4 @@ using System.Security;
 
 // The following GUID Is for the ID of the typelib if this project Is exposed to COM
 [assembly: Guid("4d6b2d82-e18c-4f26-ba83-6961d90b2706")]
-[assembly: AllowPartiallyTrustedCallers]
 //[assembly: InternalsVisibleTo("TestCases")]
